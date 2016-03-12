3d9e682e08219ea169f34c5edc6a750e8a38160b
Dmitry <syxme@mail.ru>
2016-03-12 08:28:44 +0300

    normal

--- loader.js
+++ loader.js
@@ -14,13 +14,33 @@ loader = function(){
 	vk.setToken('9e5be728c13d570fdcef85ec5f9193eb7ea1964c97bfe62ab330e496bce41629628ec788f64b30a172445');
 	vk.setSecureRequests(true);
 	vk.request('audio.get', {owner_id:65539100}, function(_o) {
-		console.log(_o.response.items);
+		items = _o.response.items;
+		item = items[items.length-1];
+		console.time('Download');
+		download(item.url,item.artist+' - '+item.title+'.mp3',function(){
+			console.timeEnd('Download');
+			console.log('end');
+		});
 	});
 
 }
 
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = http.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+};
 
+// download('http://cs4959.vk.me/u65539100/audios/9a445f63637d.mp3?extra=CHNtp7-3PsDvV763nm70nklIbQ9YCmhoVW7Efvv1vDX1w75d92P6y1WkQXwv6IQAuoGGeeFyv5ccD9Z4D-S9VDaTLhsllwgXARrzVKWdzNtY2vN41nc759uqNQXSWepHXoNSp8qJX-jj','asfsafsa.mp3',function(){
 
+// });
 
 
 module.exports = loader;
